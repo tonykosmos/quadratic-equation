9aedb75df36ce64104ddbd48bd7341fb958060ed
tonykosmos <a.kosmovskiy@gmail.com>
2018-09-28 12:00:15 +0300

    v1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+let massiv = equation.split(' ');
+let a = massiv[0];
+let m1 = massiv.slice(3,5);
+let m2 = massiv.slice(7, massiv.length);
+let b = m1.join('');
+let c = m2.join('');
+let result = [];
+let d = b*b - 4*a*c;
+let x;
+x = (-b + (Math.sqrt(d)))/(2*a);
+result.push(Math.round(x));
+x = (-b - (Math.sqrt(d)))/(2*a);
+result.push(Math.round(x));
+return result.sort((a,b)=>{return a-b});
 }
